e4802ddaa44c78852ee8aa1d51fd0e02f1950ca9
Andres Bastidas <bastidas@scoregroup.com>
2025-11-25 20:16:48 -0500

    Generated project with no starter kit.

--- config/database.php
+++ config/database.php
@@ -46,17 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'read' => [
-                'host' => [
-                    env('DB_HOST_READ', '127.0.0.1'),
-                ],
-            ],
-            'write' => [
-                'host' => [
-                    env('DB_HOST_WRITE', '127.0.0.1'),
-                ],
-            ],
-            'sticky' => true,
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
--- resources/views/components/ui/input/options/clearable.blade.php
+++
@@ -1,7 +0,0 @@
-<x-ui.input.options.button
-    x-on:click="
-        $refs.input.value = '';
-        $refs.input.dispatchEvent(new Event('input'))"
->     
-    <x-ui.icon name="x-mark" />
-</x-ui.input.options.button>
